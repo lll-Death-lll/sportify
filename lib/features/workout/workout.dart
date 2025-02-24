e19bf1fb64ee88812ec92e3ecf405da8b22a1368
Roman Avdeev <RomanAAvdeev@proton.me>
2025-02-24 13:31:47 +0400

    Added toMap and fromMap for workout class

--- lib/features/workout/workout.dart
+++ lib/features/workout/workout.dart
@@ -10,6 +10,24 @@ class Workout {
     required this.difficulty,
     required this.isOutside,
   });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'name': name,
+      'difficulty': difficulty.toInt(),
+      'is_outside': isOutside,
+    };
+  }
+
+  factory Workout.fromMap(Map<String, dynamic> map) {
+    return Workout(
+      id: map['id'],
+      name: map['name'],
+      difficulty: Difficulty.values[map['difficulty']],
+      isOutside: map['is_outside'],
+    );
+  }
 }
 
 enum Difficulty {
