1fb7e83d27af23c82d97dd4cd58d1203e03e231c
Roman Avdeev <RomanAAvdeev@proton.me>
2025-02-24 14:52:55 +0400

    Added streaks repository

--- lib/features/streak/streaks_repository.dart
+++ lib/features/streak/streaks_repository.dart
@@ -32,8 +32,58 @@ enum Rank {
         return 3;
     }
   }
+
+  factory Rank.fromStreak(int days) {
+    switch (days) {
+      case 1:
+      case 2:
+      case 3:
+      case 4:
+      case 5:
+      case 6:
+      case 7:
+        return Rank.beginner;
+      case 1:
+      case 2:
+      case 3:
+      case 4:
+      case 5:
+      case 6:
+      case 7:
+        return Rank.intermediate;
+      case 8:
+      case 9:
+      case 10:
+      case 11:
+      case 12:
+      case 13:
+      case 14:
+      case 15:
+      case 16:
+      case 17:
+      case 18:
+      case 19:
+      case 20:
+      case 21:
+        return Rank.advanced;
+      default:
+        return Rank.pro;
+    }
+  }
 }
 
 class StreaksRepository {
   final SupabaseClient _client = Supabase.instance.client;
+
+  Future<Rank> getRank(int userId) async {
+    var response =
+        await _client
+            .from('streaks')
+            .select('1')
+            .eq('user_id', userId)
+            .lte('created_at', "now() - interval '30' day")
+            .count();
+
+    return Rank.fromStreak(response.count);
+  }
 }
